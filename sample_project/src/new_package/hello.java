d466c3c927d458c1a8c445f9992e422acd269271
Administrator <Administrator@MqDSNR1-PC.MQDSNR.COM>
2020-02-12 02:45:07 +0530

    adding

---
+++ sample_project/src/new_package/hello.java
@@ -0,0 +1,30 @@
+package new_package;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.WebDriver;
+
+public class hello {
+	WebDriver driver;
+	SimpleDateFormat df;
+	Date d;
+	String time;
+	
+	public Keywords(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	
+	
+	
+	
+	/*
+	 * Keyword:-->Close Alert
+	 * Author:-->
+	 * Created on:-->
+	 * Reviewed By;-->
+	 * Last updated date:-->
+	 * 
+	 */
+}
